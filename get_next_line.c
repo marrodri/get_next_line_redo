b8a2e9a07f19240dd8a9bfbafd66d8c7fd1a974a
marrodri <laza.mar64@gmail.com>
2022-02-05 12:53:56 -0800

    chore: commented logic and pseudo code planned

---
+++ get_next_line.c
@@ -0,0 +1,21 @@
+// TODO
+/*
+Logic:
+Read the text file, and get the first line, when the function is called.
+When the function get_next_line is being called again, 
+get the next new line from the same text file. 
+
+the get_next_line will return 3 values.
+--1: if there are more bytes to read from FD.
+--1: when there is an error/
+--0: when the get_next_line, has reached 0 bytes to read.
+	aka: End Of File (EOF)
+
+
+//Pseudo code:
+first read the text file; when a new line has been founded,
+stop and store the whole line with the new line break. If there 
+are characters after the new line, store them for the next function
+call. Also return 1, for signaling that there is more bytes to read from
+the same FD.
+*/
